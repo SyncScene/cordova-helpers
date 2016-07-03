d2cd3f2abe416a5cbd855091d81fd688d2d0dba4
Andrew Male <AndyM84@users.noreply.github.com>
2016-07-03 01:24:04 -0400

    Adding support for -b flag in build.js
    -b flag will force a call to 'cordova build' as opposed to the default of 'cordova run'

--- build.js
+++ build.js
@@ -15,12 +15,24 @@ decompile(context);
 compile(context);
 
 var cmd = "cordova run";
+var cmdExtra = "";
 
 if (process.argv.length > 2) {
-	cmd += " " + process.argv.slice(2, process.argv.length).join(" ");
+	cmdExtra += " " + process.argv.slice(2, process.argv.length).join(" ");
 }
 
-console.log("Executing command: " + cmd);
+var tokens = cmdExtra.split(' ');
+
+for (var i = 0; i < tokens.length; ++i) {
+	if (tokens[i] == '-b') {
+		cmd = "cordova build";
+		tokens.splice(i, 1);
+	}
+}
+
+cmd += tokens.join(' ');
+
+console.log("executing command: " + cmd);
 
 exec(cmd, function (error, stdout, stderr) {
 	decompile(context);
